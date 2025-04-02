831dc13e459674c449376fbfd0d24f6a6dbd544e
Paxton Terry <117777648+PaxtonTerryDev@users.noreply.github.com>
2025-04-02 13:52:58 -0600

    added knex file config, basic migration

---
+++ apps/api/migrations/20250402195132_init_schema.ts
@@ -0,0 +1,50 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("organizations", (table) => {
+    table.uuid("id").primary();
+    table.string("name").notNullable();
+    table.timestamps(true, true);
+  });
+
+  await knex.schema.createTable("users", (table) => {
+    table.uuid("id").primary();
+    table.string("email").notNullable().unique();
+    table.string("password_hash").notNullable();
+    table
+      .uuid("organization_id")
+      .references("id")
+      .inTable("organizations")
+      .onDelete("CASCADE");
+    table.timestamps(true, true);
+  });
+
+  await knex.schema.createTable("projects", (table) => {
+    table.uuid("id").primary();
+    table.string("name").notNullable();
+    table
+      .uuid("organization_id")
+      .references("id")
+      .inTable("organizations")
+      .onDelete("CASCADE");
+    table.timestamps(true, true);
+  });
+
+  await knex.schema.createTable("views", (table) => {
+    table.uuid("id").primary();
+    table.string("name").notNullable();
+    table
+      .uuid("project_id")
+      .references("id")
+      .inTable("projects")
+      .onDelete("CASCADE");
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists("views");
+  await knex.schema.dropTableIfExists("projects");
+  await knex.schema.dropTableIfExists("users");
+  await knex.schema.dropTableIfExists("organizations");
+}
